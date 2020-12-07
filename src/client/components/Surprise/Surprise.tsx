f5c9fd49ebc51e09b7c833f2ee9935c346148197
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2020-12-07 13:25:40 +0200

    Integrate MUI with NextJS

--- src/client/components/Surprise/Surprise.tsx
+++ src/client/components/Surprise/Surprise.tsx
@@ -1,3 +1,10 @@
+import { Typography } from '@material-ui/core'
+import React from 'react'
+
 export default function Surprise(): JSX.Element {
-  return <div>Surprise!</div>
+  return (
+    <Typography component="h1" variant="h5">
+      Surprise!
+    </Typography>
+  )
 }
