76e2c567193ba65bf14971252967321c9b330acf
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2021-02-08 23:41:01 +0200

    Fix posts not getting populated

--- src/server/models/User.ts
+++ src/server/models/User.ts
@@ -33,7 +33,8 @@ const userSchema = new Schema({
   company: String,
   posts: [
     {
-      id: Schema.Types.ObjectId,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'post',
     },
   ],
 })
